b7d58094398f5f43ec68af6cd6609630706759a1
andreoss <andreoss@sdf.org>
2025-03-11 02:19:59 +0000

    dev: migrations and seeding on boot

---
+++ src/Idc/Migrate.hs
@@ -0,0 +1,59 @@
+module Idc.Migrate
+  ( runSchemaMigrations
+  , ensureSeeded
+  ) where
+
+import Control.Monad (unless)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Database.Persist.Sql
+import Idc.Config (Config(..))
+import Idc.Import
+import Idc.Models
+import System.FilePath ((</>))
+
+type Pool = ConnectionPool
+
+runSchemaMigrations :: Pool -> IO ()
+runSchemaMigrations pool =
+  runSqlPool (runMigration migrateAll) pool
+
+ensureSeeded :: Config -> Pool -> IO ()
+ensureSeeded cfg pool = do
+  n <- runSqlPool (count ([] :: [Filter CatalogItem])) pool
+  unless (n > 0) $ seedAll (cfgSeedDir cfg) pool
+
+seedAll :: FilePath -> Pool -> IO ()
+seedAll dir pool = do
+  c10 <- parseCatalogCsv <$> TIO.readFile (dir </> "idc10.csv")
+  c11 <- parseCatalogCsv <$> TIO.readFile (dir </> "idc11.csv")
+  xw  <- parseCrosswalkCsv <$> TIO.readFile (dir </> "crosswalk.csv")
+  runSqlPool (insertMany_ (toItems Idc10 c10 ++ toItems Idc11 c11 ++ toXw xw)) pool
+
+toItems :: Catalog -> [Row] -> [CatalogItem]
+toItems cat = map toItem
+  where
+    toItem (Row c p t e) =
+      CatalogItem
+        { itemCatalog  = cat
+        , itemCode     = c
+        , itemParent   = p
+        , itemTitle    = t
+        , itemChapter  = nonEmpty e
+        }
+
+nonEmpty :: Text -> Maybe Text
+nonEmpty e = if T.null e then Nothing else Just e
+
+toXw :: [(Text, Text, Text)] -> [Crosswalk]
+toXw = map toOne
+  where
+    toOne (a, b, k) = Crosswalk a b (readKind k)
+
+readKind :: Text -> MapKind
+readKind k = case T.toLower k of
+  "equal"    -> Equal
+  "narrower" -> Narrower
+  "broader"  -> Broader
+  _          -> Related
