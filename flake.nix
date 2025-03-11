6e9e34e089e2abb9025079d1570d2414370e58a2
andreoss <andreoss@sdf.org>
2025-03-11 01:51:59 +0000

    arch: scaffold project per adr-01

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "idc-catalog REST services";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAll = f: nixpkgs.lib.genAttrs systems (s: f s);
+      hpFor = pkgs: pkgs.haskell.packages.ghc96;
+    in
+    {
+      packages = forAll (sys:
+        let pkgs = nixpkgs.legacyPackages.${sys};
+        in {
+          default = (hpFor pkgs).callCabal2nix "idc-catalog" ./. { };
+        });
+
+      apps = forAll (sys:
+        let pkgs = nixpkgs.legacyPackages.${sys};
+        in {
+          default = {
+            type = "app";
+            program = "${self.packages.${sys}.default}/bin/idc-catalog";
+          };
+        });
+
+      devShells = forAll (sys:
+        let pkgs = nixpkgs.legacyPackages.${sys};
+            hp = hpFor pkgs;
+        in {
+          default = pkgs.mkShell {
+            buildInputs = [
+              hp.ghc
+              hp.cabal-install
+              (hp.haskell-language-server.override { })
+              pkgs.postgresql_16
+              pkgs.redis
+              pkgs.curl
+              pkgs.pkg-config
+              pkgs.zlib
+            ];
+          };
+        });
+    };
+}
