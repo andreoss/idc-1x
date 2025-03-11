e601a7f96c0d6f0b89abbfe0f41dd559e65e6c5c
andreoss <andreoss@sdf.org>
2025-03-11 23:01:03 +0000

    dev: wrap db with runNoLoggingT and runResourceT

--- src/Idc/Api.hs
+++ src/Idc/Api.hs
@@ -6,6 +6,8 @@ module Idc.Api
   ) where
 
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Logger (runNoLoggingT)
+import Control.Monad.Trans.Resource (runResourceT)
 import Data.Aeson (Value, object, (.=))
 import qualified Data.ByteString.Lazy as BL
 import Data.Maybe (fromMaybe, listToMaybe)
@@ -75,7 +77,7 @@ failWith :: ServerError -> Text -> Handler a
 failWith e t = throwError e { errBody = BL.fromStrict (encodeUtf8 t) }
 
 db :: Env -> SqlPersistM a -> IO a
-db env act = runSqlPool act (envPool env)
+db env act = runResourceT $ runNoLoggingT $ runSqlPool act (envPool env)
 
 listItems :: Env -> Text -> Maybe Int -> Maybe Int -> Maybe Text -> Handler Value
 listItems env tag mpage mperPage mparent = do
